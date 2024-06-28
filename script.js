0adcc487e38f7ed942494c2da4c935607ca50c47
aarya <baarya24@tbc.edu.np>
2024-06-29 01:07:53 +0545

    Finalizing the skeleton

---
+++ script.js
@@ -0,0 +1,28 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+autoSlide();
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slide");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+function autoSlide() {
+  plusSlides(1);
+  setTimeout(autoSlide, 3000);
+}
